166f3d3023a356b51bdc629a8d62da2ec85b74b3
fcakiroglu16 <f-cakiroglu@outlook.com>
2026-04-09 21:55:44 +0300

    3.gün tamamlandı.

---
+++ RestAprilEducationRepository.Persistence/ProductRepositoryWithInMemory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestAprilEducationRepository.Application.Products;
+using RestAprilEducationRepository.Domain;
+
+namespace RestAprilEducationRepository.Persistence
+{
+    public class ProductRepositoryWithInMemory : IProductRepository
+    {
+        public Task<List<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> CreateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> UpdateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetAllWithPagedAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- RestAprilEducationRepository/Program.cs
+++ RestAprilEducationRepository/Program.cs
@@ -1,3 +1,8 @@
+using RestAprilEducationRepository.API.Endpoints.Products;
+using RestAprilEducationRepository.Application.Products;
+using RestAprilEducationRepository.Persistence;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +11,31 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//DI Container Framework ( Library ) IoC Container Framework
+
+//  DI+ IoC =>  DI Pattern
+
+//Singleton
+//Scoped
+//Transient
+//builder.Services.AddSingleton<CalculateService>();
+
+// Transient > Scoped > Singleton
+builder.Services.AddSingleton<ICalculateService, CalculateService>();
+builder.Services.AddScoped<IProductsApplication, ProductsApplication>();
+builder.Services.AddScoped<IProductRepository, ProductRepositoryWithInMemory>();
+
+
 var app = builder.Build();
+
+
+app.AddProductEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
