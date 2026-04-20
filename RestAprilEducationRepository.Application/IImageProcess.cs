01adccf0460b6276f94c8fa3708c68e756e721d0
fcakiroglu16 <f-cakiroglu@outlook.com>
2026-04-20 21:34:19 +0300

    Add custom exception handling and related endpoints
    Implemented BusinessException and UserFriendlyException with detailed handlers. Registered handlers in Program.cs and added a demo endpoint. Updated Product to use BusinessException for price validation. Introduced IImageProcess interface and implementation.

---
+++ RestAprilEducationRepository.Application/IImageProcess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestAprilEducationRepository.Application
+{
+    internal interface IImageProcess
+    {
+        void Process(string path);
+    }
+
+    public class ImageProcess : IImageProcess
+    {
+        public void Process(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                // 
+
+                throw new Exception("path değeri boş olamaz");
+            }
+
+            // Görüntü işleme kodları burada olacak
+        }
+    }
+}
--- RestAprilEducationRepository/Program.cs
+++ RestAprilEducationRepository/Program.cs
@@ -1,10 +1,13 @@
 using FluentValidation;
+using RestAprilEducationRepository.API.Endpoints.ExceptionHandlerExamples;
 using RestAprilEducationRepository.API.Endpoints.Products;
 using RestAprilEducationRepository.API.Endpoints.Versioning;
+using RestAprilEducationRepository.API.ExceptionsHandlers;
 using RestAprilEducationRepository.API.Extensions;
 using RestAprilEducationRepository.Application;
 using RestAprilEducationRepository.Application.Products;
 using RestAprilEducationRepository.Application.Products.Create;
+using RestAprilEducationRepository.Domain.Exceptions;
 using RestAprilEducationRepository.Persistence;
 using Scalar.AspNetCore;
 
@@ -35,12 +38,20 @@ builder.Services.AddRepositoriesExt();
 
 builder.Services.AddValidatorsFromAssemblyContaining<ApplicationAssembly>();
 builder.Services.AddVersioningExt();
+
+
+builder.Services.AddExceptionHandler<UserFriendlyExceptionHandler>().AddExceptionHandler<BusinessExceptionHandler>()
+    .AddExceptionHandler<GlobalExceptionHandler>();
+
 var app = builder.Build();
 
+app.UseExceptionHandler(options => { });
 
 var apiVersionSet = app.AddVersionSetExt();
 app.AddProductEndpoints(apiVersionSet);
 app.AddVersionExampleEndpoints(apiVersionSet);
+
+app.AddExceptionHandlerExampleEndpoint();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
