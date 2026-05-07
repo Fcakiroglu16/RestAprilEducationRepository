c394f11714311e01e6f2ae58d93bf53e6d75d815
fcakiroglu16 <f-cakiroglu@outlook.com>
2026-05-07 21:48:21 +0300

    gRPC lesson

---
+++ GrpcServer/Repositories/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GrpcServer.Repositories;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TodoItem>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Title)
+                  .IsRequired()
+                  .HasMaxLength(200);
+        });
+    }
+}
